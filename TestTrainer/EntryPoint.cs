26321d1c37adf5d803e631ac9f0524d8c81ec3e9
Tim Kaiser <timkaiser3@web.de>
2024-10-23 21:19:19 +0200

    added test project

---
+++ TestTrainer/EntryPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTrainer;
+
+internal static class EntryPoint
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+    }
+}
---
+++ TestTrainer/TestTrainer.cs
@@ -0,0 +1,18 @@
+﻿using ReadWriteMemory;
+
+namespace TestTrainer;
+
+public sealed class TestTrainer
+{
+    private readonly RwMemory _memory = new("Ai");
+
+    public TestTrainer()
+    {
+        _memory.ProcessOnStateChanged += OnProcessOnStateChanged;
+    }
+
+    private void OnProcessOnStateChanged(bool newprocessstate)
+    {
+        Console.WriteLine($"ProcessOnStateChanged: {newprocessstate}");
+    }
+}
