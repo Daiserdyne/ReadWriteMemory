84a868741ce6cac4017e49c13de9622dcbefdbf2
Daiserdyne <timkaiser3@web.de>
2024-12-01 22:32:09 +0100

    Started implementing interal rw memory.

---
+++ ReadWriteMemory.Internal/RwMemory.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using ReadWriteMemory.Shared.Entities;
+
+namespace ReadWriteMemory.Internal;
+
+/// <summary>
+/// <inheritdoc cref="ReadWriteMemory"/>
+/// </summary>
+public partial class RwMemory
+{
+    private readonly Dictionary<MemoryAddress, MemoryAddressTable> _memoryRegister = [];
+    private readonly Dictionary<string, nuint> _modules = [];
+    
+    private void GetAllLoadedProcessModules()
+    {
+        var processModules = Process.GetCurrentProcess().Modules
+            .Cast<ProcessModule>()
+            .ToList();
+
+        foreach (var module in processModules)
+        {
+            var moduleName = module.ModuleName.ToLower();
+
+            if (!_modules.ContainsKey(moduleName))
+            {
+                _modules.Add(moduleName, (nuint)module.BaseAddress);
+                continue;
+            }
+
+            _modules[moduleName] = (nuint)module.BaseAddress;
+        }
+    }
+
+    private unsafe nuint GetTargetAddress(MemoryAddress memoryAddress)
+    {
+        var baseAddress = GetBaseAddress(memoryAddress);
+
+        var targetAddress = baseAddress;
+        
+        if (memoryAddress.Offsets is not null && memoryAddress.Offsets.Any())
+        {
+            for (uint i = 0; i < memoryAddress.Offsets.Length - 1; i++)
+            {
+                targetAddress = nuint.Add(targetAddress, memoryAddress.Offsets[i]);
+                targetAddress = *(nuint*)targetAddress;
+            }
+            
+            targetAddress = nuint.Add(targetAddress, 
+                memoryAddress.Offsets[memoryAddress.Offsets.Length - 1]);
+        }
+
+        if (!_memoryRegister.ContainsKey(memoryAddress))
+        {
+            _memoryRegister.Add(memoryAddress, new()
+            {
+                BaseAddress = baseAddress
+            });
+        }
+
+        return targetAddress;
+    }
+    
+    private unsafe nuint GetBaseAddress(MemoryAddress memoryAddress)
+    {
+        if (_memoryRegister.TryGetValue(memoryAddress, out var value)
+            && value.BaseAddress != nuint.Zero)
+        {
+            return _memoryRegister[memoryAddress].BaseAddress;
+        }
+        
+        var moduleAddress = nuint.Zero;
+
+        var moduleName = memoryAddress.ModuleName;
+        
+        if (!string.IsNullOrEmpty(moduleName))
+        {
+            _modules.TryGetValue(moduleName, out moduleAddress);
+        }
+
+        var address = memoryAddress.Address;
+
+        if (moduleAddress != nuint.Zero)
+        {
+            return *(nuint*)(moduleAddress + address);
+        }
+
+        return *(nuint*)memoryAddress.Address;
+    }
+}
---
+++ ReadWriteMemory.Internal/WriteMemory.cs
@@ -0,0 +1,38 @@
+﻿using ReadWriteMemory.Shared.Entities;
+
+namespace ReadWriteMemory.Internal;
+
+public partial class RwMemory
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="memoryAddress"></param>
+    /// <param name="bytes"></param>
+    public unsafe void WriteBytes(MemoryAddress memoryAddress, byte[] bytes)
+    {
+        var targetAddress = GetTargetAddress(memoryAddress);
+        
+        fixed (byte* sourcePtr = bytes)
+        {
+            var destPtr = (byte*)targetAddress;
+            
+            Buffer.MemoryCopy(sourcePtr, destPtr, 
+                bytes.Length, bytes.Length);
+        }
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="memoryAddress"></param>
+    /// <param name="value"></param>
+    public unsafe void WriteValue<T>(MemoryAddress memoryAddress, T value) where T : unmanaged
+    {
+        var targetAddress = GetTargetAddress(memoryAddress);
+        
+        var destPtr = (T*)targetAddress;
+
+        *destPtr = value;
+    }
+}
