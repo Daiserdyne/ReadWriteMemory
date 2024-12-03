9a876ca8bf997f904c0f98d3e16ae94cc9dde413
Daiserdyne <timkaiser3@web.de>
2024-12-03 22:56:16 +0100

    started adding call function method prototype

---
+++ ReadWriteMemory.Internal/CallFunction.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using ReadWriteMemory.Shared.Entities;
+
+namespace ReadWriteMemory.Internal;
+
+public partial class RwMemory
+{
+    public unsafe Out CallFunction<In, In2, Out>(MemoryAddress memoryAddress, CallingConvention convention,
+        In arg1, In2 arg2, Out arg3)
+    {
+        var targetAddress = GetTargetAddress(memoryAddress);
+        
+        var func = (delegate* unmanaged[Cdecl]<In, In2, Out>)targetAddress;
+
+        return func(arg1, arg2);
+    }
+}
