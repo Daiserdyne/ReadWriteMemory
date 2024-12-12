305b02850dd934cb0ba630dcc7c6a78e6605616d
Tim Kaiser <tim.kaiser@elementlogic.de>
2024-12-12 16:52:39 +0100

    added callfunction t4 file to generate the different variations of the calling conventions for the Functioncalling

--- TestTrainer.Internal.InjectMe/EntryPoint.cs
+++ TestTrainer.Internal.InjectMe/EntryPoint.cs
@@ -3,19 +3,19 @@ using System.Runtime.InteropServices;
 
 namespace TestTrainer.Internal.InjectMe;
 
-public static class EntryPoint
+public static partial class EntryPoint
 {
-    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)], EntryPoint = nameof(DllMain))]
+    public static ReadOnlySpan<byte> Mogus => "Amogus"u8;
+    
+    [LibraryImport("user32.dll")]
+    private static partial int MessageBoxA(nint hWnd, ReadOnlySpan<byte> text, ReadOnlySpan<byte> caption, uint type);
+    
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvFastcall)], EntryPoint = nameof(DllMain))]
     public static bool DllMain(nint module, uint reason, nint reserved)
     {
         if (reason == 1)
         {
-            _ = Task.Run(() =>
-            {
-                new SignalTrainer().Main(CancellationToken.None)
-                    .GetAwaiter()
-                    .GetResult();
-            });
+            MessageBoxA(nint.Zero, Mogus, "Christus"u8, 0x000000100);
         }
         
         return true; 
