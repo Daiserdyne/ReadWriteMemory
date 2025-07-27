af91aae94f5a46a4cc766cadea25d8740cc46a72
Daiserdyne <timkaiser3@web.de>
2025-07-27 23:05:57 +0200

    Removed typos

--- ReadWriteMemory.External/Interfaces/IMemoryTrainer.cs
+++ ReadWriteMemory.External/Interfaces/IMemoryTrainer.cs
@@ -8,7 +8,7 @@ namespace ReadWriteMemory.External.Interfaces;
 public interface IMemoryTrainer
 {
     /// <summary>
-    /// Specifies the trainer by an unique id. This is usefull when you want to order/sort a list of trainer.
+    /// Specifies the trainer by a unique id. This is useful when you want to order/sort a list of trainer.
     /// </summary>
     public int Id { get; }
     
